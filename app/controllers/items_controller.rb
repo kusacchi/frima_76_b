9060799912ab84968bbd2b00e0e6e8f13ba8dea3
yofio <d.y215jump@i.softbank.jp>
2020-09-20 15:19:08 +0900

    コメントアウト消去

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,5 @@
 class ItemsController < ApplicationController
   def index
-    # @exhibitions = Exhibition.find(1)
     @exhibitions = Exhibition.all.limit(5).order("created_at DESC")
-    # @images = Image.all
   end
 end
