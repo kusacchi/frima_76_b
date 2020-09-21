27c0a17ef82e7bfd16b43be924f6844907702fce
kusacchi <66767439+kusacchi@users.noreply.github.com>
2020-09-21 12:39:46 +0900

    Update show_exhibition.js

--- app/assets/javascripts/show_exhibition.js
+++ app/assets/javascripts/show_exhibition.js
@@ -1,6 +1,6 @@
-$(document).on('turbolinks:load', function () {
-  $('.show-subimage').click(function () {
-    var $url = $(this).attr('image');
-    $('.show-image').attr('image', $url);
-  });
-});
+// $(document).on('turbolinks:load', function () {
+//   $('.show-subimage').click(function () {
+//     var $url = $(this).attr('image');
+//     $('.show-image').attr('image', $url);
+//   });
+// });
