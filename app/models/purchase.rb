68913d9f72cfed47a0f36c9b95509852809a00db
kazu8901 <ka_8901@docomo.ne.jp>
2020-10-11 19:43:22 +0900

    purchese_create

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,9 +1,12 @@
 class Purchase < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
 
-  validates :post_code, presence: true
-  validates :prefecture_code, presence: true
-  validates :city, presence: true
-  validates :address, presence: true
-  validates :user_id, presence: true 
+  with_options presence: true do
+    validates :post_code, presence: true, numericality: { only_integer: true }
+    validates :prefecture_code, presence: true
+    validates :city, presence: true
+    validates :address, presence: true
+    validates :user_id, presence: true 
+  end
+  validates :phone_number, numericality: { only_integer: true }, allow_blank: true
 end
